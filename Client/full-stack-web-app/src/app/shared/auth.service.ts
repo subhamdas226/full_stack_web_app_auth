88e5a61f72c4738829574a60b7982973f176fccd
Subham Das <53463561+subhamdas226@users.noreply.github.com>
2023-05-25 16:51:07 +0530

    Update auth.service.ts

--- Client/full-stack-web-app/src/app/shared/auth.service.ts
+++ Client/full-stack-web-app/src/app/shared/auth.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -16,16 +17,24 @@ export class AuthService {
 
   constructor(private http:HttpClient) { }
   
-  
+  private url = `http://localhost:3100/api`;
   private nregisterUrl = "http://localhost:3100/api/register";
   private loginUrl = "http://localhost:3100/api/login"
  
   registerCandidate(candidate:any) {
     
-    return this.http.post<any>(this.nregisterUrl,candidate)
+    return this.http.post<any>(this.nregisterUrl,candidate,{
+      withCredentials: true
+    })
   } 
   
   loginUser(user:any){
-    return this.http.post<any>(this.loginUrl, user)
+    return this.http.post<any>(this.loginUrl, user, {
+      withCredentials: true
+    })
+  }
+
+  isAuthenticated(): Observable<any> {
+    return this.http.get(this.url + "/protected", { withCredentials: true });
   }
 }
